e3d21256ffc7e608e682ce2ab34e434ac55131ef
MehmetEminYagli <m.eminyagli@gmail.com>
2024-10-31 20:14:36 +0300

    dosyalar düzenlendi

---
+++ Assets/_GameDev/Script/MachinePaletScript1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MachinePaletScript1 : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
