a6a0139c06070d9d87d00e5d7e12781ea70705e1
MehmetEminYagli <m.eminyagli@gmail.com>
2024-11-04 23:06:17 +0300

    yeni assetsler eklendi nesneler doğru ve yanlış ise olan sistem değiştirildi ve doğru ise makine efekti eklendi

--- Assets/_GameDev/Script/ItemController.cs
+++ Assets/_GameDev/Script/ItemController.cs
@@ -1,73 +1,62 @@
 using UnityEngine;
 using DG.Tweening;
-
 public class ItemController : MonoBehaviour
 {
-    [SerializeField] private MachineController machine;
 
-    private Vector3 initialScale;
-    private Rigidbody itemRb;
+
+    [SerializeField] private MachineController machine;
+    [SerializeField] private GameObject controlMachineObject;
+    [SerializeField] private GameObject trueMachineEffect;
+    [SerializeField] private GameObject falseMachineEffect;
+
 
     private void Start()
     {
-
         machine = GetComponentInParent<MachineController>();
+        SetTrueMachineEffect(false);
+        SetFalseMachineEffect(false);
     }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent<FactoryItem>(out FactoryItem item))
+        if (other.TryGetComponent<FactoryItem>(out FactoryItem factoryItem))
         {
-            initialScale = item.transform.localScale;
-            itemRb = item.GetComponent<Rigidbody>();
+            Vector3 initialScale = factoryItem.transform.localScale;
+            SetTrueMachineEffect(false);
+            SetFalseMachineEffect(false);
 
-            if (item.GetItemID() == machine.GetMachineID())
+            if (factoryItem.GetItemID() == machine.GetMachineID())
             {
-                Debug.Log("item id ile machine id'i doğru +1 puan ver");
-                if (itemRb != null)
-                {
-                    itemRb.useGravity = false;
-                }
-
-                item.transform.DOMoveY(transform.position.y + .2f, .3f).SetEase(Ease.OutBack);
-                item.transform.DOScale(initialScale * 1.3f, .3f).SetEase(Ease.OutBack)
-                .OnComplete(() =>
-                {
-                    if (itemRb != null)
-                    {
-                        itemRb.useGravity = true;
-                    }
-                    item.transform.DOScale(initialScale, .2f).SetEase(Ease.InOutQuad).OnComplete(() =>
-                    {
-                        item.transform.DOScale(Vector3.zero, .5f).SetEase(Ease.InOutQuad).SetDelay(2f).OnComplete(() =>
-                        {
-                            Destroy(item);
-                        });
-                    });
-                });
+                SetTrueMachineEffect(true);
             }
-            //item id ile machine id ayni degil wronggg destroy ettt kız salla bisi yap iste
             else
             {
-                Debug.Log("WRONGGGGGGGG");
-                if (itemRb != null)
+                SetFalseMachineEffect(true);
+                factoryItem.transform.DOScale(Vector3.zero, .5f).SetEase(Ease.InOutQuad).SetDelay(.5f).OnComplete(() =>
                 {
-                    itemRb.useGravity = false;
-                }
-
-                item.transform.DOMoveY(transform.position.y + .2f, .3f).SetEase(Ease.OutBack);
-                item.transform.DOScale(initialScale * 1.3f, .3f).SetEase(Ease.OutBack)
-                    .OnComplete(() =>
-                    {
-                        item.transform.DOShakePosition(0.3f, new Vector3(0.2f, 0, 0)).SetLoops(3, LoopType.Yoyo);
-                        item.transform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InBack).SetDelay(0.5f)
-                        .OnComplete(() =>
-                        {
-                            Destroy(item);
-                        });
-                    });
+                    Destroy(factoryItem);
+                    SetTrueMachineEffect(false);
+                    SetFalseMachineEffect(false);
+                });
             }
         }
     }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<FactoryItem>(out FactoryItem factoryItem))
+        {
+            SetTrueMachineEffect(false);
+            SetFalseMachineEffect(false);
+        }
+    }
+    private void SetTrueMachineEffect(bool isActive)
+    {
+        trueMachineEffect.SetActive(isActive);
+    }
+    private void SetFalseMachineEffect(bool isActive)
+    {
+        falseMachineEffect.SetActive(isActive);
+    }
 
 }
+
+//eski sistemdede nesne yukarı kalkıyor sallanıyor sonra yoluna devam ediyordu. bunları kaldırıp bunu yerine makine ekledim makine efekt çıkarsın istiyorum. #region //private Vector3 initialScale; //private Rigidbody itemRb; //private void Start() //{ // machine = GetComponentInParent<MachineController>(); //} //private void OnTriggerEnter(Collider other) //{ // if (other.TryGetComponent<FactoryItem>(out FactoryItem item)) // { // initialScale = item.transform.localScale; // itemRb = item.GetComponent<Rigidbody>(); // if (item.GetItemID() == machine.GetMachineID()) // { // Debug.Log("item id ile machine id'i doğru +1 puan ver"); // if (itemRb != null) // { // itemRb.useGravity = false; // } // item.transform.DOMoveY(transform.position.y + .2f, .3f).SetEase(Ease.OutBack); // item.transform.DOScale(initialScale * 1.3f, .3f).SetEase(Ease.OutBack) // .OnComplete(() => // { // if (itemRb != null) // { // itemRb.useGravity = true; // } // item.transform.DOScale(initialScale, .2f).SetEase(Ease.InOutQuad).OnComplete(() => // { // item.transform.DOScale(Vector3.zero, .5f).SetEase(Ease.InOutQuad).SetDelay(2f).OnComplete(() => // { // Destroy(item); // }); // }); // }); // } // //item id ile machine id ayni degil wronggg destroy ettt kız salla bisi yap iste // else // { // Debug.Log("WRONGGGGGGGG"); // if (itemRb != null) // { // itemRb.useGravity = false; // } // item.transform.DOMoveY(transform.position.y + .2f, .3f).SetEase(Ease.OutBack); // item.transform.DOScale(initialScale * 1.3f, .3f).SetEase(Ease.OutBack) // .OnComplete(() => // { // item.transform.DOShakePosition(0.3f, new Vector3(0.2f, 0, 0)).SetLoops(3, LoopType.Yoyo); // item.transform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InBack).SetDelay(0.5f) // .OnComplete(() => // { // Destroy(item); // }); // }); // } // } //} #endregion }
