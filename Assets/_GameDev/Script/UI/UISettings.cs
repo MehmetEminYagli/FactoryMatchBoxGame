d6fae510833c5415e458476c15ed998d9d627192
MehmetEminYagli <m.eminyagli@gmail.com>
2024-11-29 18:51:02 +0300

    settings panel and haptic system engtegre edildi

--- Assets/_GameDev/Script/UI/UISettings.cs
+++ Assets/_GameDev/Script/UI/UISettings.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class UISettings : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private ToggleButton hapticToggle;
+    [SerializeField] private ToggleButton soundToggle;
+    [SerializeField] private ToggleButton musicToggle;
+
+    private void Start()
     {
-        
+        hapticToggle.SetValue(DataManager.Vibration);
+        soundToggle.SetValue(DataManager.Sound);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnSoundToggled(bool value)
     {
-        
+        DataManager.Sound = value;
+        DataManager.Music = value;
+
+        GameManager.Instance.audioManager.UpdateAudioStates();
+    }
+
+    public void OnMusicToggled(bool value)
+    {
+        DataManager.Music = value;
+
+        GameManager.Instance.audioManager.UpdateAudioStates();
+    }
+
+    public void OnHapticToggled(bool value)
+    {
+        DataManager.Vibration = value;
+        HapticManager.SetHapticsActive(value);
     }
 }
